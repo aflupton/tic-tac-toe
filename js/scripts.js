7eef258070fa538779adae6ae876de37e6b78093
Andrew Lupton, Brian Henry and David Tumpowsky <andrew@>
2018-04-05 16:12:16 -0700

    click function uses text rather than append to input Xs and Os

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,34 @@
 // Player O
 var player = {
   player1: "X",
-  player2: "0",
+  player2: "O",
 }
 
 
 
-player.player1
 
 
 
 
 
 
-function Space(space) {
-  this.spaceId = space;
-}
-
-function WinningCombo (index1, index2, index3) {
-  var horizontal1 = WinningCombo (space1, space2, space3);
-}
-
-
+// function Space(space) {
+//   this.spaceId = space;
+// }
+//
+// function WinningCombo (index1, index2, index3) {
+//   var horizontal1 = WinningCombo (space1, space2, space3);
+//   var horizontal1 = WinningCombo (space1 && space2 && space3);
+//   var horizontal2 = WinningCombo (space4 && space5 && space6);
+//   var horizontal3 = WinningCombo (space7 && space8 && space9);
+//   var verticle1 = WinningCombo (space1 && space4 && space7);
+//   var verticle2 = WinningCombo (space2 && space5 && space8);
+//   var verticle3 = WinningCombo (space3 && space6 && space9);
+//   var diagnol1 = WinningCombo (space1 && space5 && space9);
+//   var diagnol2 = WinningCombo (space3 && space5 && space7);
+// }
+//
+//
 // var horizontal1 = (space1 && space2 && space3);
 // var horizontal2 = (space4 && space5 && space6);
 // var horizontal3 = (space7 && space8 && space9);
@@ -35,17 +42,24 @@ function WinningCombo (index1, index2, index3) {
 
 
 
+
+
 $(document).ready(function() {
   var currentPlayer = 'player1';
   $(".grid-item").click(function() {
-    $(this).append(player[currentPlayer]);
-    if (currentPlayer === 'player1') {
-      currentPlayer = 'player2';
-    } else {
-      currentPlayer = 'player1';
-    }
+    $(this).text(player[currentPlayer]);
+      if (currentPlayer === 'player1') {
+        currentPlayer = 'player2';
+      } else {
+        currentPlayer = 'player1';
+      }
+
+
+
     $("#turn1").toggle();
     $("#turn2").toggle();
   });
+
+
 
 });
